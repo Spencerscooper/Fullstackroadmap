ce205dfa8602b132f79e5b97ea4190d1aac50905
Spencerscooper <spencercooper1991@gmail.com>
2023-12-06 23:00:04 +0000

    Squaring number answer

--- Sahnun-Task/Lesson1/Lesson2-Dict.py
+++ Sahnun-Task/Lesson1/Lesson2-Dict.py
@@ -6,5 +6,18 @@ bio = {"Spencer": 5, "Sahnu":3, "Jeff" : 2, "Freena": 1}
 
 search = bio.items()
 for names in search:
+    print(type(search))
     print(names)
+
+# Question write a program that takes in an aray of integers and print out each
+# integer squared to the second power)
+
+
+#def print_squared(numbers):
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    square = number ** 2
+    print(f"The square of\t {number} \t {square}")
+
+
 
